0e674a198a26fb46d47532a932071b2ab7b46381
xierenhong <xierenhong@kuaishou.com>
2022-12-27 14:45:16 +0800

    chore: add test demo

---
+++ src/components/Guide/Guide.tsx
@@ -0,0 +1,26 @@
+import { Layout, Row, Typography } from 'antd';
+import React from 'react';
+import styles from './Guide.less';
+
+interface Props {
+  name: string;
+ [key: string]: any;
+}
+
+// 脚手架示例组件
+const Guide: React.FC<Props> = (props) => {
+  const { name, ...otherProps } = props;
+
+  console.log(otherProps)
+  return (
+    <Layout>
+      <Row>
+        <Typography.Title level={3} className={styles.title} {...otherProps}>
+          欢迎使用 <strong>{name}</strong> ！
+        </Typography.Title>
+      </Row>
+    </Layout>
+  );
+};
+
+export default Guide;
